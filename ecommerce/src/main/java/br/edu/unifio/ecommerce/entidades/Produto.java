e9da4c2424d092ca1ace83147b37c2a9b575f042
Guilherme Ribeiro <guilhermeroliveira04@gmail.com>
2026-09-01 11:39:21 +0000

    Commit Entidades

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
@@ -1,21 +1,33 @@
 package br.edu.unifio.ecommerce.entidades;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 @Getter
 @Setter
-public class Categoria {
+public class Produto {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Short id;
+    private Integer id;
 
     private String nome;
 
     private String descricao;
+
+    private Short estoque;
+
+    private BigDecimal preco;
+
+    @ManyToOne
+    private Categoria categoria; 
 }
---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into categoria (nome, descricao) values ('Pets', 'Brinquedos de Pets');
+
+insert into categoria (nome, descricao) values ('Jogos', 'Jogos RPG');
+
+insert into categoria (nome, descricao) values ('Elétrica', 'Cabos de Cobre');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Cabo de Cobre 4mm', 'Rolo 100m', 550, 20, 2);
